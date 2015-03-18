9a62634b86be6afd10bfaee4c2163b10729ffe39
Sam Boonen <sam.boonen@intracto.com>
2015-03-18 18:09:30 +0100

    Print song title in room.

--- bot/handlers/RadioHandler.php
+++ bot/handlers/RadioHandler.php
@@ -2,6 +2,7 @@
 
 namespace Handlers;
 
+use GuzzleHttp\Client as GuzzleClient;
 
 class RadioHandler
 {
@@ -17,16 +18,37 @@ class RadioHandler
     {
         global $config;
 
-        if (!preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $message['message'], $match)) {
+        if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $message['message'], $match)) {
+          if(!isset($match[1])){
             throw new \InvalidArgumentException("Doesn't match youtube url");
+          }
+          $video_id = $match[1];
         }
+
+        if (!isset($video_id)) {
+          throw new \InvalidArgumentException("Doesn't match youtube url");
+        }
+
+        $client = new GuzzleClient();
+
+        $response = $client->get(
+          'http://gdata.youtube.com/feeds/api/videos/' . $video_id,
+          [
+            'headers' => ['Content-Type' => 'text/xml'],
+            'verify' => false,
+            'timeout' => 5,
+          ]
+        );
+
+        $xml = $response->xml();
+
 
         makePost($config['radioUrl'] . 'add', [
             'link' => $message['message'],
             'requestname' => $message['from']['name'],
         ]);
 
-        return array("action" => "reply", "data" => 'Added ' . $message['message'] . ' to radio');
+        return array("action" => "reply", "data" => 'Added "' . $xml->title . '" to Radio Wizi');
 
     }
 }

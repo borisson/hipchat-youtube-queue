59776084b28946469fadc14a26af62fe6eae11c8
Sam Boonen <sam.boonen@intracto.com>
2015-03-22 11:30:39 +0100

    Remove debug data

--- site/web/js/src/controllers.js
+++ site/web/js/src/controllers.js
@@ -6,14 +6,8 @@ var radiowiziControllers = angular.module('radiowiziControllers', []);
 
 radiowiziControllers.controller('mainController', ['$scope', '$http',
     function($scope, $http) {
-        //$scope.lastSongs = [
-        //    {'title': 'Nexus S'},
-        //    {'title': 'Motorola XOOM™ with Wi-Fi'},
-        //    {'title': 'MOTOROLA XOOM™'},
-        //    {'title': 'MOTOROLA XOOM™ 2'},
-        //
-        //];
         $http.get(origin + '/' + folder + '/api/loadlastsongs').success(function(data) {
             $scope.lastsongs = data;
         });
-    }]);
+    }
+]);

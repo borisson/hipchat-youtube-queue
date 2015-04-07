5fe17ef7a1a0c91e3dae3d89ce3e3a9e86648092
Senne Torfs <torfssenne@gmail.com>
2015-04-07 21:43:36 +0200

    Tabs are now working.

--- site/web/js/src/controllers.js
+++ site/web/js/src/controllers.js
@@ -170,5 +170,13 @@ radiowiziControllers.controller('MainController', [ '$scope', '$http', '$interva
             });
         });
 
+      $scope.tab = 1;
+      $scope.selectTab = function (setTab){
+        $scope.tab = setTab;
+      };
+      $scope.isSelected = function(checkTab) {
+        return $scope.tab === checkTab;
+      };
+
     }
 ]);

77a93e841337a4a55e5965e838a15a6ab4e1f57e
Sam Boonen <sam.boonen@intracto.com>
2015-04-13 18:07:23 +0200

    We changed the 'random' query a little bit to make it more random and pleasant to listen to.

--- site/src/AppBundle/Entity/Repository/YoutubeMovieRepository.php
+++ site/src/AppBundle/Entity/Repository/YoutubeMovieRepository.php
@@ -6,52 +6,50 @@ use Doctrine\ORM\EntityRepository;
 
 class YoutubeMovieRepository extends EntityRepository
 {
-    public function findRandomTopSong()
-    {
-        $query = "SELECT id,
-    	COUNT(id) AS num,
-    	(
-        	SELECT count(b.id) as countMax
-        	FROM youtube_movies as b
-        	WHERE title != 'jingle'
-        	GROUP BY video_id
-        	ORDER BY countMax DESC
-        	LIMIT 1
-    	) AS max
+  public function findRandomTopSong()
+  {
+    $query = "SELECT *
         FROM `youtube_movies`
-        WHERE title != 'jingle'
+        WHERE title != 'Jingle'
             AND length < 1800
+            AND skipped = 0
+            AND requestname <> 'Random top hit'
+            AND requestname <> 'Radio wizi'
+            AND requestname <> 'Ultra Wizi TOP 10'
         GROUP BY video_id
-        HAVING num > max / 5
         ORDER BY RAND()
         LIMIT 1";
 
-        $connection = $this->getEntityManager()->getConnection();
-        $statement = $connection->prepare($query);
-        $statement->execute();
-        $result = $statement->fetch();
-        return $this->find($result['id']);
-    }
+    $connection = $this->getEntityManager()->getConnection();
+    $statement = $connection->prepare($query);
+    $statement->execute();
+    $result = $statement->fetch();
+    return $this->find($result['id']);
+  }
 
-    public function getTop10Songs()
-    {
-        $query = "SELECT id, COUNT(id) as num
+  public function getTop10Songs()
+  {
+    $query = "SELECT id, COUNT(id) as num
         FROM `youtube_movies`
-        WHERE title != 'jingle'
+        WHERE title != 'Jingle'
             AND length < 1800
+            AND skipped = 0
+            AND requestname <> 'Random top hit'
+            AND requestname <> 'Radio wizi'
+            AND requestname <> 'Ultra Wizi TOP 10'
         GROUP BY video_id
         ORDER BY num DESC LIMIT 0,9";
 
-        $connection = $this->getEntityManager()->getConnection();
-        $statement = $connection->prepare($query);
-        $statement->execute();
-        $results = $statement->fetchAll();
+    $connection = $this->getEntityManager()->getConnection();
+    $statement = $connection->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll();
 
-        $output = [];
-        foreach ($results as $row) {
-            $output[] = $this->find($row['id']);
-        }
-        return $output;
+    $output = [];
+    foreach ($results as $row) {
+      $output[] = $this->find($row['id']);
     }
+    return $output;
+  }
 
 }
